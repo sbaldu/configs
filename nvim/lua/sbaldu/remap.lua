5e65fcd13f976a29cf5c91187c8c9df7d668a135
sbaldu <simone.balducci00@gmail.com>
2023-11-09 17:36:23 +0100

    Update escape keybinding nvim

--- nvim/lua/sbaldu/remap.lua
+++ nvim/lua/sbaldu/remap.lua
@@ -1,5 +1,6 @@
 
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>es", vim.cmd.Ex)
+-- vim.keymap.set("n", "<leader>es", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>es", ":q<CR>")
 vim.keymap.set("n", "<", "<<")
 vim.keymap.set("n", ">", ">>")
