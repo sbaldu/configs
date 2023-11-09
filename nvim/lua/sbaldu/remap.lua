5d0b67bd855cd7c5876c403ca64b6c19c1c76243
sbaldu <simone.balducci00@gmail.com>
2023-11-09 17:36:43 +0100

    Add nvim bindings for moving between windows

--- nvim/lua/sbaldu/remap.lua
+++ nvim/lua/sbaldu/remap.lua
@@ -4,3 +4,9 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>es", ":q<CR>")
 vim.keymap.set("n", "<", "<<")
 vim.keymap.set("n", ">", ">>")
+
+-- keybindings for moving between windows
+vim.keymap.set("n", "<leader>a", "<C-w>h")
+vim.keymap.set("n", "<leader>d", "<C-w>l")
+vim.keymap.set("n", "<leader>s", "<C-w>j")
+vim.keymap.set("n", "<leader>w", "<C-w>k")
