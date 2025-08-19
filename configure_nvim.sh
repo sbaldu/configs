9259587ed5920e73896bf860ef351a4690210e79
Simone Balducci <simone.balducci00@gmail.com>
2025-08-19 17:06:57 +0200

    Add small script for automatically configuring nvim

---
+++ configure_nvim.sh
@@ -0,0 +1,30 @@
+#
+
+if [ -z "$1" ]; then
+	echo "Usage: $0 <user_name> [install_path]"
+	exit 1
+fi
+
+user_name="${1:-user}"
+install_path="${2:-$HOME}"
+
+## save configs in .config folder
+mkdir -p ~/.config/nvim && cp -r nvim ~/.config
+mv ~/.config/nvim/lua/sbaldu ~/.config/nvim/lua/$user_name
+echo "require('${user_name}')" > ~/.config/nvim/init.lua
+find ~/.config/nvim/lua/$user_name -type f -name "*.lua" -exec sed -i "s/"sbaldu"/${user_name}/g" {} +
+
+
+## install neovim
+cd ~ ;
+git clone https://github.com/neovim/neovim && cd neovim && make -j 16 CMAKE_INSTALL_PREFIX=$HOME && make install
+cd ~  && echo "alias nvim=~/neovim/build/bin/nvim" >> ~/.bashrc && source ~/.bashrc
+
+## install packer
+git clone --depth 1 https://github.com/wbthomason/packer.nvim \
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+## install neovim packages
+mv ~/.config/nvim/after ~/.config
+cd ~/.config/nvim/lua/${user_name} && nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerInstall'
+mv ~/.config/after ~/.config/nvim/
