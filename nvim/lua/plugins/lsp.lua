cea5ee8e9efb2dec458a746cd9ceb6262d9a4c06
Simone Balducci <simone.balducci00@gmail.com>
2025-10-31 16:46:09 +0100

    Improve completion in LSP configuration

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -14,16 +14,22 @@ return {
 
   -- Main LSP plugin
   {
-	  "neovim/nvim-lspconfig",
-	  config = function()
-	  vim.lsp.config("clangd", {
-		cmd = { "clangd", "--background-index", "--clang-tidy" },
-		filetypes = { "c", "cpp", "cuda" },
-	  })
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-	  vim.lsp.enable("clangd")
+      require("lspconfig").clangd.setup({
+        cmd = { "clangd", "--background-index", "--clang-tidy" },
+        capabilities = capabilities, -- 👈 connects cmp and LSP
+        filetypes = { "c", "cpp", "cuda" },
+        root_dir = require("lspconfig.util").root_pattern(
+          "compile_commands.json",
+          ".git",
+          "CMakeLists.txt"
+        ),
+      })
 
-      -- Keymaps for LSP actions
+      -- LSP keymaps
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
       vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
       vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
@@ -32,30 +38,44 @@ return {
     end,
   },
 
-  -- autocompletion dependencies
+  -- Autocompletion
   {
-	'hrsh7th/nvim-cmp'
-  },
-  {
-	'hrsh7th/cmp-buffer'
-  },
-  {
-	'hrsh7th/cmp-path'
-  },
-  {
-	'hrsh7th/cmp-nvim-lsp'
-  },
-  {
-	'hrsh7th/cmp-nvim-lua'
-  },
-  {
-	'saadparwaiz1/cmp_luasnip'
-  },
-  {
-	'rafamadriz/friendly-snippets'
-  },
-  {
-	'L3MON4D3/LuaSnip'
-  },
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lua",
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+      "L3MON4D3/LuaSnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
 
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
+  },
 }
