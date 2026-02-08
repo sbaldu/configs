ffba038d5fcc8a952b4ae40820ee9afe619fa19a
Simone Balducci <simone.balducci00@gmail.com>
2026-02-08 20:33:13 +0100

    Add toggleterm and undotree plugins

---
+++ nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,21 @@
+return {
+  'akinsho/toggleterm.nvim',
+  config = function()
+		require('toggleterm').setup{
+			-- size = 20,
+			-- open_mapping = [[<c-\>]],
+			-- shade_filetypes = {},
+			shade_terminals = true,
+			shading_factor = 2,
+			-- start_in_insert = true,
+			-- insert_mappings = true,
+			-- persist_size = true,
+			direction = 'float',
+			-- close_on_exit = true,
+			-- shell = vim.o.shell,
+		}
+	end,
+
+  -- ToggleTerm toggle mapping
+  vim.api.nvim_set_keymap('n', '<leader>o', ':ToggleTerm<CR>', { noremap = true, silent = true })
+}
